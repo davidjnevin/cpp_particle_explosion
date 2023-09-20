36ddc5d124fca9cf713360c6f61b54a9c7ecf66a
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-09-20 21:53:27 +0200

    Refactor screen to Screen.h and.cpp

---
+++ SDL_BASIC/include/Screen.cpp
@@ -0,0 +1,75 @@
+#include "Screen.h"
+
+namespace davidjnevin {
+
+Screen::Screen()
+    : m_window(NULL), m_renderer(NULL), m_texture(NULL), m_buffer(NULL) {}
+
+bool Screen::init() {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("SDL_Init Error: %s\n", SDL_GetError());
+    return false;
+  }
+  m_window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (m_window == NULL) {
+    printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return false;
+  }
+
+  m_renderer = SDL_CreateRenderer(
+      m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  if (m_renderer == NULL) {
+    SDL_DestroyWindow(m_window);
+    printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return false;
+  }
+
+  m_texture =
+      SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888,
+                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  if (m_texture == NULL) {
+    SDL_DestroyRenderer(m_renderer);
+    SDL_DestroyWindow(m_window);
+    printf("SDL_CreateTexture Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return false;
+  }
+
+  m_buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+  // could check for null pointer here
+
+  // Set all pixels to white
+  // Using hexidecimal notation for colors to make it easier to read
+  memset(m_buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+  // Set some pixels to green
+  // Using hexidecimal notation for colors to make it easier to read
+  for (int i = 0; i < (SCREEN_WIDTH * SCREEN_HEIGHT); i++) {
+    m_buffer[i] = 0x000000FF;
+  }
+
+  // buffer[3000] = 0xFFFFFFFF;
+  SDL_UpdateTexture(m_texture, NULL, m_buffer, SCREEN_WIDTH * sizeof(Uint32));
+  SDL_RenderClear(m_renderer);
+  SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
+  SDL_RenderPresent(m_renderer);
+  return true;
+}
+
+bool Screen::processEvents() { return false; }
+
+void Screen::close() {
+  delete[] m_buffer;
+  SDL_DestroyRenderer(m_renderer);
+  SDL_DestroyTexture(m_texture);
+  SDL_DestroyWindow(m_window);
+  SDL_Quit();
+}
+
+} /* namespace davidjnevin */
--- SDL_BASIC/main.cpp
+++ SDL_BASIC/main.cpp
@@ -3,56 +3,16 @@
 
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  const int SCREEN_WIDTH = 640;
-  const int SCREEN_HEIGHT = 480;
+#include "Screen.h"
 
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    printf("SDL_Init Error: %s\n", SDL_GetError());
-    return 1;
+using namespace davidjnevin;
+
+int main() {
+  Screen screen;
+
+  if (screen.init() == false) {
+    std::cout << "Error initializing SDL." << std::endl;
   }
-  SDL_Window *window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED,
-                                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                        SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (window == NULL) {
-    printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-
-  SDL_Renderer *renderer = SDL_CreateRenderer(
-      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-  if (renderer == NULL) {
-    SDL_DestroyWindow(window);
-    printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-
-  SDL_Texture *texture =
-      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-  if (texture == NULL) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    printf("SDL_CreateTexture Error: %s\n", SDL_GetError());
-    SDL_Quit();
-    return 1;
-  }
-
-  Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
-  // could check for null pointer here
-
-  // Set all pixels to white
-  // Using hexidecimal notation for colors to make it easier to read
-  memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
-
-  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
-  SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, texture, NULL, NULL);
-  SDL_RenderPresent(renderer);
 
   bool quit = false;
   SDL_Event event;
@@ -69,10 +29,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  delete[] buffer;
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyTexture(texture);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  screen.close();
+
   return 0;
 }
