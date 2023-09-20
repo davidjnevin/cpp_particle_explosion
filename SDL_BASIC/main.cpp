107936a126462c59d430aa15f4f56468a36c354d
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-09-20 15:11:16 +0200

    Using memset for white screen

--- SDL_BASIC/main.cpp
+++ SDL_BASIC/main.cpp
@@ -1,12 +1,78 @@
 #include <SDL.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  const int SCREEN_WIDTH = 640;
+  const int SCREEN_HEIGHT = 480;
+
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     printf("SDL_Init Error: %s\n", SDL_GetError());
     return 1;
   }
+  SDL_Window *window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                        SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+    printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  if (renderer == NULL) {
+    SDL_DestroyWindow(window);
+    printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Texture *texture =
+      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  if (texture == NULL) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    printf("SDL_CreateTexture Error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+  // could check for null pointer here
+
+  // Set all pixels to white
+  // Using hexidecimal notation for colors to make it easier to read
+  memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
+  bool quit = false;
+  SDL_Event event;
+
+  // Game loop
+  while (!quit) {
+    // Update particles
+    // Draw particles
+    // Check for messages/events
+    while (SDL_PollEvent(&event) != 0) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+  }
+
+  delete[] buffer;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyTexture(texture);
+  SDL_DestroyWindow(window);
   SDL_Quit();
-  printf("SDL2 initialized and quit successfully!\n");
   return 0;
 }
